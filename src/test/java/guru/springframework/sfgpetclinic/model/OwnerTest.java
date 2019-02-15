5e78c895a48cd9d24a3e9b57b1ac9a2c6589e538
krlosh <krlosh@gmail.com>
2019-02-15 17:27:27 +0100

    Use inner assertions with lambdas

---
+++ src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
@@ -0,0 +1,25 @@
+package guru.springframework.sfgpetclinic.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OwnerTest {
+
+    @Test
+    void dependantAssertions() {
+        Owner ownerTest = new Owner(1l, "Joe", "Buck");
+        ownerTest.setCity("Key West");
+        ownerTest.setTelephone("12312312322");
+        assertAll("Properties test",
+                ()-> assertAll("Person properties",
+                        () -> assertEquals("Joe", ownerTest.getFirstName(), "First name did not match"),
+                        () -> assertEquals("Buck", ownerTest.getLastName(), "Last name did not match")
+                ),
+                ()->assertAll("Owner properties",
+                        () -> assertEquals("Key West",ownerTest.getCity(), "City did not match"),
+                        () -> assertEquals("12312312322",ownerTest.getTelephone(), "Telephone did not match")
+                )
+        );
+    }
+}
