83e5d89576b659470ba9190fe5fc83dd03cc8197
Carlos Henche <carlos.henche@digibis.com>
2019-02-15 09:22:47 +0100

    Junit assertions example

---
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
@@ -0,0 +1,29 @@
+package guru.springframework.sfgpetclinic.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IndexControllerTest {
+
+    IndexController controller ;
+
+    @BeforeEach
+    void setUp() {
+        this.controller = new IndexController();
+    }
+
+    @Test
+    void index() {
+        //Also lambda expression should be used but although it's only evaluated in failure conditions it's more
+        // expensive
+        assertEquals("index", controller.index(),"Wrong view returned");
+    }
+
+    @Test
+    void oupsHandler() {
+        assertTrue("asdf".equals(controller.oupsHandler()),()->"This is some expensive " + "Message to build" + " for" +
+                " my test");
+    }
+}
