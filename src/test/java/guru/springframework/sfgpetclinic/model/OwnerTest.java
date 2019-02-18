d3f01a8e57b6a769d9a3d49487faa8b2d6cc3efe
krlosh <krlosh@gmail.com>
2019-02-18 11:42:15 +0100

    Customize display name

--- src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
@@ -1,6 +1,7 @@
 package guru.springframework.sfgpetclinic.model;
 
 import guru.springframework.sfgpetclinic.ModelTests;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -30,7 +31,8 @@ class OwnerTest implements ModelTests {
         assertThat(ownerTest.getCity(), is("Key West"));
     }
 
-    @ParameterizedTest
+    @DisplayName("Value Source test")
+    @ParameterizedTest(name="{displayName} - [{index}] {arguments}")
     @ValueSource(strings = {"One" , "Two", "Three"})
     public void testParameterizedTest(String val){
         System.out.println("VALUE: " + val);
